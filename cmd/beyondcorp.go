b0e859ae5da86baa9459496e0db30a94192f82b4
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 16:36:12 +0100

    Add beyondcorp command stubs (#306)

---
+++ cmd/beyondcorp.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud beyondcorp (#306) ---
+
+var beyondcorpCmd = &cobra.Command{
+	Use:   "beyondcorp",
+	Short: "Manage BeyondCorp resources (stubbed)",
+}
+
+func init() {
+	registerStubGroup(beyondcorpCmd, "operations", "Manage BeyondCorp operations", "describe", "list", "cancel", "delete")
+	registerStubGroup(beyondcorpCmd, "security-gateways", "Manage BeyondCorp security gateways", "create", "delete", "describe", "list", "update")
+	rootCmd.AddCommand(beyondcorpCmd)
+}
