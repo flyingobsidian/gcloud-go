342e428cb69a730720cd2e04f2b0c510c131fe98
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 18:08:58 +0100

    Add network-connectivity command stubs (#361)

---
+++ cmd/network_connectivity.go
@@ -0,0 +1,23 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud network-connectivity (#361) ---
+
+var networkConnectivityCmd = &cobra.Command{Use: "network-connectivity", Short: "Manage Network Connectivity (stubbed)"}
+
+func init() {
+	crud := []string{"create", "delete", "describe", "list", "update"}
+	registerStubGroup(networkConnectivityCmd, "hubs", "Manage hubs", append(crud, "accept-spoke", "reject-spoke", "list-spokes")...)
+	registerStubGroup(networkConnectivityCmd, "internal-ranges", "Manage internal ranges", crud...)
+	registerStubGroup(networkConnectivityCmd, "locations", "Get locations", "list", "describe")
+	registerStubGroup(networkConnectivityCmd, "multicloud-data-transfer-configs", "Manage multicloud data transfer configs", crud...)
+	registerStubGroup(networkConnectivityCmd, "multicloud-data-transfer-supported-services", "Manage supported services", "describe", "list")
+	registerStubGroup(networkConnectivityCmd, "operations", "Manage operations", "cancel", "delete", "describe", "list")
+	registerStubGroup(networkConnectivityCmd, "policy-based-routes", "Manage policy-based routes", "create", "delete", "describe", "list")
+	registerStubGroup(networkConnectivityCmd, "regional-endpoints", "Manage regional endpoints", "create", "delete", "describe", "list")
+	registerStubGroup(networkConnectivityCmd, "service-connection-policies", "Manage service connection policies", crud...)
+	registerStubGroup(networkConnectivityCmd, "spokes", "Manage spokes", append(crud, "accept", "reject")...)
+	registerStubGroup(networkConnectivityCmd, "transports", "Manage transports", crud...)
+	rootCmd.AddCommand(networkConnectivityCmd)
+}
