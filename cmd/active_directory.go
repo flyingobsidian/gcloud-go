513877ef86e48e8547f70d82a09a3108b8528b05
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 18:35:33 +0100

    Add active-directory command stubs (#289)

---
+++ cmd/active_directory.go
@@ -0,0 +1,15 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud active-directory (#289) ---
+
+var activeDirectoryCmd = &cobra.Command{Use: "active-directory", Short: "Manage Managed Microsoft AD (stubbed)"}
+
+func init() {
+	crud := []string{"create", "delete", "describe", "list", "update"}
+	registerStubGroup(activeDirectoryCmd, "domains", "Manage AD domains", append(crud, "attach-trust", "detach-trust", "reset-managed-identities-admin-password", "restore", "backup", "sudoers")...)
+	registerStubGroup(activeDirectoryCmd, "operations", "Manage AD operations", "cancel", "describe", "list")
+	registerStubGroup(activeDirectoryCmd, "peerings", "Manage AD peerings", crud...)
+	rootCmd.AddCommand(activeDirectoryCmd)
+}
