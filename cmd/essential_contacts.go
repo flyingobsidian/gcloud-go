06de5bef93e79dabda3eb243d14bcdbbc4c394b4
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 17:29:16 +0100

    Add essential-contacts command stubs (#337)

---
+++ cmd/essential_contacts.go
@@ -0,0 +1,14 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud essential-contacts (#337) ---
+
+var essentialContactsCmd = &cobra.Command{Use: "essential-contacts", Short: "Manage essential contacts (stubbed)"}
+
+func init() {
+	for _, name := range []string{"compute", "create", "delete", "describe", "list", "update"} {
+		registerStubCommand(essentialContactsCmd, name, "Not yet implemented")
+	}
+	rootCmd.AddCommand(essentialContactsCmd)
+}
