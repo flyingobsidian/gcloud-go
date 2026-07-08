4ff913d016239b705a5e016f48fc29bd677f8adc
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 17:22:39 +0100

    Add edge-cache command stubs (#333)

---
+++ cmd/edge_cache.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud edge-cache (#333) ---
+
+var edgeCacheCmd = &cobra.Command{Use: "edge-cache", Short: "Manage Media CDN Edge Cache (stubbed)"}
+
+func init() {
+	crud := []string{"create", "delete", "describe", "list", "update"}
+	registerStubGroup(edgeCacheCmd, "keysets", "Manage EdgeCacheKeyset resources", crud...)
+	registerStubGroup(edgeCacheCmd, "operations", "Manage operations", "describe", "list", "cancel", "delete")
+	registerStubGroup(edgeCacheCmd, "origins", "Manage EdgeCacheOrigin resources", crud...)
+	registerStubGroup(edgeCacheCmd, "services", "Manage EdgeCacheService resources", crud...)
+	rootCmd.AddCommand(edgeCacheCmd)
+}
