77ac333ce8e14b62ae63770811a5f63a0f8dcb61
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 18:48:52 +0100

    Add apigee command stubs (#297)

---
+++ cmd/apigee.go
@@ -0,0 +1,21 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud apigee (#297) ---
+
+var apigeeCmd = &cobra.Command{Use: "apigee", Short: "Manage Apigee (stubbed)"}
+
+func init() {
+	crud := []string{"create", "delete", "describe", "list", "update"}
+	registerStubGroup(apigeeCmd, "apis", "Manage API proxies", "delete", "deploy", "describe", "list", "undeploy")
+	registerStubGroup(apigeeCmd, "applications", "Manage applications", "describe", "list")
+	registerStubGroup(apigeeCmd, "archives", "Manage archives", crud...)
+	registerStubGroup(apigeeCmd, "deployments", "Manage deployments", "describe", "list")
+	registerStubGroup(apigeeCmd, "developers", "Manage developers", crud...)
+	registerStubGroup(apigeeCmd, "environments", "Manage environments", crud...)
+	registerStubGroup(apigeeCmd, "operations", "Manage operations", "describe", "list")
+	registerStubGroup(apigeeCmd, "organizations", "Manage organizations", "describe", "list", "provision")
+	registerStubGroup(apigeeCmd, "products", "Manage API products", crud...)
+	rootCmd.AddCommand(apigeeCmd)
+}
