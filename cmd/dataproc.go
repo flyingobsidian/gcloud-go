70ae265ca1e3ee8013a5c7742547af6cb2b7eb1b
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 17:06:50 +0100

    Add dataproc command stubs (#324)

---
+++ cmd/dataproc.go
@@ -0,0 +1,19 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud dataproc (#324) ---
+
+var dataprocCmd = &cobra.Command{Use: "dataproc", Short: "Manage Dataproc (stubbed)"}
+
+func init() {
+	crud := []string{"create", "delete", "describe", "list", "update"}
+	registerStubGroup(dataprocCmd, "autoscaling-policies", "Manage autoscaling policies", append(crud, "import", "export", "get-iam-policy", "set-iam-policy")...)
+	registerStubGroup(dataprocCmd, "batches", "Submit batch jobs", append(crud, "cancel", "submit", "wait")...)
+	registerStubGroup(dataprocCmd, "clusters", "Manage clusters", append(crud, "diagnose", "start", "stop", "import", "export", "get-iam-policy", "set-iam-policy")...)
+	registerStubGroup(dataprocCmd, "jobs", "Manage jobs", append(crud, "submit", "kill", "wait")...)
+	registerStubGroup(dataprocCmd, "node-groups", "Manage node groups", append(crud, "resize", "repair")...)
+	registerStubGroup(dataprocCmd, "operations", "Manage operations", "cancel", "delete", "describe", "list", "wait")
+	registerStubGroup(dataprocCmd, "workflow-templates", "Manage workflow templates", append(crud, "instantiate", "instantiate-from-file", "import", "export", "add-job", "remove-job", "set-managed-cluster", "set-cluster-selector", "run")...)
+	rootCmd.AddCommand(dataprocCmd)
+}
