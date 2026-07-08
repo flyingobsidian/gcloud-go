ac2e1e45cc8d70311dc6b7ae638175219a6e9b9b
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 17:53:10 +0100

    Add looker command stubs (#351)

---
+++ cmd/looker.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud looker (#351) ---
+
+var lookerCmd = &cobra.Command{Use: "looker", Short: "Manage Looker (stubbed)"}
+
+func init() {
+	crud := []string{"create", "delete", "describe", "list", "update"}
+	registerStubGroup(lookerCmd, "backups", "Manage backups", crud...)
+	registerStubGroup(lookerCmd, "instances", "Manage instances", append(crud, "import", "export", "restart", "restore")...)
+	registerStubGroup(lookerCmd, "operations", "Manage operations", "cancel", "describe", "list")
+	registerStubGroup(lookerCmd, "regions", "Manage regions", "list")
+	rootCmd.AddCommand(lookerCmd)
+}
