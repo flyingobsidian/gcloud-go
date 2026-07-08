9a2086926f5c7f0be2df107ee30d86247354c1ec
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 19:00:40 +0100

    Add recommender command stubs (#379)

---
+++ cmd/recommender.go
@@ -0,0 +1,15 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud recommender (#379) ---
+
+var recommenderCmd = &cobra.Command{Use: "recommender", Short: "Manage Cloud Recommender (stubbed)"}
+
+func init() {
+	registerStubGroup(recommenderCmd, "insight-type-config", "Manage insight type configuration", "describe", "update")
+	registerStubGroup(recommenderCmd, "insights", "Manage insights", "describe", "list", "mark-accepted", "mark-active", "mark-dismissed")
+	registerStubGroup(recommenderCmd, "recommendations", "Manage recommendations", "describe", "list", "mark-active", "mark-claimed", "mark-dismissed", "mark-failed", "mark-succeeded")
+	registerStubGroup(recommenderCmd, "recommender-config", "Manage recommender configuration", "describe", "update")
+	rootCmd.AddCommand(recommenderCmd)
+}
