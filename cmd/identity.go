73ce035cbec5fd5d03c16eabe96b644be9e01673
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 17:44:43 +0100

    Add identity command stubs (#346)

---
+++ cmd/identity.go
@@ -0,0 +1,18 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud identity (#346) ---
+
+var identityCmd = &cobra.Command{Use: "identity", Short: "Manage Cloud Identity (stubbed)"}
+
+func init() {
+	groups := &cobra.Command{Use: "groups", Short: "Manage Cloud Identity Groups"}
+	for _, n := range []string{"create", "delete", "describe", "list", "update", "search", "get-iam-policy", "set-iam-policy"} {
+		registerStubCommand(groups, n, "Not yet implemented")
+	}
+	registerStubGroup(groups, "memberships", "Manage group memberships", "create", "delete", "describe", "list", "update", "search-transitive-memberships", "search-transitive-groups", "modify-membership-roles")
+	registerStubGroup(groups, "preview", "Preview commands", "list")
+	identityCmd.AddCommand(groups)
+	rootCmd.AddCommand(identityCmd)
+}
