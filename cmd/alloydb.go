9bf47dfd6b06b4e65b42fb05d0d253ad822ac6c5
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 18:42:20 +0100

    Add alloydb command stubs (#293)

---
+++ cmd/alloydb.go
@@ -0,0 +1,17 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud alloydb (#293) ---
+
+var alloydbCmd = &cobra.Command{Use: "alloydb", Short: "Manage AlloyDB (stubbed)"}
+
+func init() {
+	crud := []string{"create", "delete", "describe", "list", "update"}
+	registerStubGroup(alloydbCmd, "backups", "Manage backups", crud...)
+	registerStubGroup(alloydbCmd, "clusters", "Manage clusters", append(crud, "promote", "restart", "restore", "switchover", "upgrade", "migrate-cloud-sql", "export", "import")...)
+	registerStubGroup(alloydbCmd, "instances", "Manage instances", append(crud, "restart", "failover", "inject-fault", "execute-sql")...)
+	registerStubGroup(alloydbCmd, "operations", "Manage operations", "cancel", "describe", "list")
+	registerStubGroup(alloydbCmd, "users", "Manage users", crud...)
+	rootCmd.AddCommand(alloydbCmd)
+}
