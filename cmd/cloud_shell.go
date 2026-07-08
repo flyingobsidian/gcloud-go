b727dd69dfd60f7eb521c55f26b5c10ec051f8fc
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 16:50:01 +0100

    Add cloud-shell command stubs (#314)

---
+++ cmd/cloud_shell.go
@@ -0,0 +1,14 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud cloud-shell (#314) ---
+
+var cloudShellCmd = &cobra.Command{Use: "cloud-shell", Short: "Manage Cloud Shell (stubbed)"}
+
+func init() {
+	for _, name := range []string{"get-mount-command", "scp", "ssh"} {
+		registerStubCommand(cloudShellCmd, name, "Not yet implemented")
+	}
+	rootCmd.AddCommand(cloudShellCmd)
+}
