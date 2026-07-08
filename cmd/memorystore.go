c026e18b66a3d0272ce79210d4a081c6e1ca0982
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 17:59:36 +0100

    Add memorystore command stubs (#355)

---
+++ cmd/memorystore.go
@@ -0,0 +1,17 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud memorystore (#355) ---
+
+var memorystoreCmd = &cobra.Command{Use: "memorystore", Short: "Manage Memorystore for Valkey (stubbed)"}
+
+func init() {
+	crud := []string{"create", "delete", "describe", "list", "update"}
+	registerStubGroup(memorystoreCmd, "acl-policies", "Manage ACL policies", crud...)
+	registerStubGroup(memorystoreCmd, "backup-collections", "Manage backup collections", crud...)
+	registerStubGroup(memorystoreCmd, "instances", "Manage instances", append(crud, "backup", "reschedule-maintenance", "failover", "certificate-authorities")...)
+	registerStubGroup(memorystoreCmd, "locations", "Manage locations", "list", "describe")
+	registerStubGroup(memorystoreCmd, "operations", "Manage operations", "cancel", "describe", "list")
+	rootCmd.AddCommand(memorystoreCmd)
+}
