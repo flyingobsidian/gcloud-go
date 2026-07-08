ccc98dd12a97211900dd6e70804e3f18f1605d80
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 17:46:27 +0100

    Add ids command stubs (#347)

---
+++ cmd/ids.go
@@ -0,0 +1,12 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud ids (#347) ---
+
+var idsCmd = &cobra.Command{Use: "ids", Short: "Manage Cloud IDS (stubbed)"}
+
+func init() {
+	registerStubGroup(idsCmd, "endpoints", "Manage IDS endpoints", "create", "delete", "describe", "list", "update")
+	rootCmd.AddCommand(idsCmd)
+}
