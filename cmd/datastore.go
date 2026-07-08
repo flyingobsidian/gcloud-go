1ebd53d794a6a952674f29474161c6439d2fdc95
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 17:08:40 +0100

    Add datastore command stubs (#325)

---
+++ cmd/datastore.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud datastore (#325) ---
+
+var datastoreCmd = &cobra.Command{Use: "datastore", Short: "Manage Cloud Datastore (stubbed)"}
+
+func init() {
+	registerStubGroup(datastoreCmd, "indexes", "Manage Datastore indexes", "create", "list", "cleanup")
+	registerStubGroup(datastoreCmd, "operations", "Manage operations", "cancel", "delete", "describe", "list")
+	for _, name := range []string{"export", "import"} {
+		registerStubCommand(datastoreCmd, name, "Not yet implemented")
+	}
+	rootCmd.AddCommand(datastoreCmd)
+}
