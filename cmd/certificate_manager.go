7e21a14dcf7a3c369110749a189155d802bc7ee7
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 16:48:15 +0100

    Add certificate-manager command stubs (#313)

---
+++ cmd/certificate_manager.go
@@ -0,0 +1,18 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud certificate-manager (#313) ---
+
+var certificateManagerCmd = &cobra.Command{Use: "certificate-manager", Short: "Manage Certificate Manager (stubbed)"}
+
+func init() {
+	crud := []string{"create", "delete", "describe", "list", "update"}
+	registerStubGroup(certificateManagerCmd, "certificates", "Manage certificates", crud...)
+	registerStubGroup(certificateManagerCmd, "dns-authorizations", "Manage DNS authorizations", crud...)
+	registerStubGroup(certificateManagerCmd, "issuance-configs", "Manage issuance configs", crud...)
+	registerStubGroup(certificateManagerCmd, "maps", "Manage certificate maps", crud...)
+	registerStubGroup(certificateManagerCmd, "operations", "Manage operations", "describe", "list")
+	registerStubGroup(certificateManagerCmd, "trust-configs", "Manage trust configs", crud...)
+	rootCmd.AddCommand(certificateManagerCmd)
+}
