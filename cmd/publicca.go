08e01c6546c14fe104e06629b59277f53e22c795
ZanNen <github3746@ashleygc.co.uk>
2026-07-08 18:55:32 +0100

    Add publicca command stubs (#376)

---
+++ cmd/publicca.go
@@ -0,0 +1,12 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud publicca (#376) ---
+
+var publiccaCmd = &cobra.Command{Use: "publicca", Short: "Manage Google Trust Services PublicCA (stubbed)"}
+
+func init() {
+	registerStubGroup(publiccaCmd, "external-account-keys", "Manage ACME external account keys", "create")
+	rootCmd.AddCommand(publiccaCmd)
+}
