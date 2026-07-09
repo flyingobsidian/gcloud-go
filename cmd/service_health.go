1ee6186a8f402f3a75944b0f33e24dfb1d99cc93
ZanNen <github3746@ashleygc.co.uk>
2026-07-09 13:30:48 +0100

    Add service-health command stubs (#384)

---
+++ cmd/service_health.go
@@ -0,0 +1,14 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud service-health (#384) ---
+
+var serviceHealthCmd = &cobra.Command{Use: "service-health", Short: "Manage Service Health (stubbed)"}
+
+func init() {
+	registerStubGroup(serviceHealthCmd, "events", "Manage events", "describe", "list")
+	registerStubGroup(serviceHealthCmd, "organization-events", "Manage organization events", "describe", "list")
+	registerStubGroup(serviceHealthCmd, "organization-impacts", "Manage organization impacts", "describe", "list")
+	rootCmd.AddCommand(serviceHealthCmd)
+}
