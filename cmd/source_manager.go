123805324a3cfd16a2f75d2ca9624f7745ac873d
ZanNen <github3746@ashleygc.co.uk>
2026-07-09 14:30:10 +0100

    Add source-manager command stubs (#386)

---
+++ cmd/source_manager.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud source-manager (#386) ---
+
+var sourceManagerCmd = &cobra.Command{Use: "source-manager", Short: "Manage Secure Source Manager (stubbed)"}
+
+func init() {
+	crud := []string{"create", "delete", "describe", "list", "update"}
+	registerStubGroup(sourceManagerCmd, "instances", "Manage instances", crud...)
+	registerStubGroup(sourceManagerCmd, "locations", "Manage locations", "describe", "list")
+	registerStubGroup(sourceManagerCmd, "operations", "Manage operations", "cancel", "delete", "describe", "list")
+	registerStubGroup(sourceManagerCmd, "repos", "Manage repositories", append(crud, "get-iam-policy", "set-iam-policy", "add-iam-policy-binding", "remove-iam-policy-binding")...)
+	rootCmd.AddCommand(sourceManagerCmd)
+}
