caeead70a67562a3852a1baa854569c7f9d31b72
ZanNen <github3746@ashleygc.co.uk>
2026-07-09 14:44:06 +0100

    Add vector-search command stubs (#394)

---
+++ cmd/vector_search.go
@@ -0,0 +1,14 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// --- gcloud vector-search (#394) ---
+
+var vectorSearchCmd = &cobra.Command{Use: "vector-search", Short: "Manage Vector Search (stubbed)"}
+
+func init() {
+	registerStubGroup(vectorSearchCmd, "collections", "Manage collections",
+		"create", "delete", "describe", "list", "update", "upload-records", "delete-records", "query")
+	registerStubGroup(vectorSearchCmd, "operations", "Manage operations", "cancel", "delete", "describe", "list")
+	rootCmd.AddCommand(vectorSearchCmd)
+}
