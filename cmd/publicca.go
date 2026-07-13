93efea40743bbcdcb4aa82f7407e24417f15a02d
ZanNen <github3746@ashleygc.co.uk>
2026-07-13 13:45:12 +0100

    Register missing publicca subcommands, drop stubbed tag (#647)

--- cmd/publicca.go
+++ cmd/publicca.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 // --- gcloud publicca (#376) ---
 
-var publiccaCmd = &cobra.Command{Use: "publicca", Short: "Manage Google Trust Services PublicCA (stubbed)"}
+var publiccaCmd = &cobra.Command{Use: "publicca", Short: "Manage Google Trust Services PublicCA"}
 
 func init() {
 	registerStubGroup(publiccaCmd, "external-account-keys", "Manage ACME external account keys", "create")
