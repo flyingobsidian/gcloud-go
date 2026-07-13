e5432cdd2b9b69ffab0da0a323e2ccc8098b6201
ZanNen <github3746@ashleygc.co.uk>
2026-07-13 12:24:27 +0100

    Register missing lustre subcommands, drop stubbed tag (#626)

--- cmd/lustre.go
+++ cmd/lustre.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 // --- gcloud lustre (#352) ---
 
-var lustreCmd = &cobra.Command{Use: "lustre", Short: "Manage Lustre (stubbed)"}
+var lustreCmd = &cobra.Command{Use: "lustre", Short: "Manage Lustre"}
 
 func init() {
 	registerStubGroup(lustreCmd, "instances", "Manage Lustre instances", "create", "delete", "describe", "list", "update", "import-data", "export-data")
