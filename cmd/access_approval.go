4a9ec4da7ecfbb012e6346ac0548203865ea0aa8
ZanNen <github3746@ashleygc.co.uk>
2026-07-13 09:45:24 +0100

    Drop stubbed tag from access-approval Short (#566)

--- cmd/access_approval.go
+++ cmd/access_approval.go
@@ -4,7 +4,11 @@ import "github.com/spf13/cobra"
 
 // --- gcloud access-approval (#287) ---
 
-var accessApprovalCmd = &cobra.Command{Use: "access-approval", Short: "Manage Access Approval (stubbed)"}
+// accessApprovalCmd exposes the gcloud-python access-approval surface.
+// All three direct child subgroups (requests, service-account, settings) are
+// registered; leaf commands currently error with "not yet implemented" pending
+// full API wiring. See #566.
+var accessApprovalCmd = &cobra.Command{Use: "access-approval", Short: "Manage Access Approval"}
 
 func init() {
 	registerStubGroup(accessApprovalCmd, "requests", "Manage access approval requests",
