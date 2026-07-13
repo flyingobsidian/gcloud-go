6b0022bccf4161bd78d9883def30590c975e49f2
ZanNen <github3746@ashleygc.co.uk>
2026-07-13 09:57:28 +0100

    Register missing bq subcommands, drop stubbed tag (#585)

--- cmd/bq.go
+++ cmd/bq.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 // --- gcloud bq (#311) ---
 
-var bqCmd = &cobra.Command{Use: "bq", Short: "Manage BigQuery Migration resources (stubbed)"}
+var bqCmd = &cobra.Command{Use: "bq", Short: "Manage BigQuery Migration resources"}
 
 func init() {
 	registerStubGroup(bqCmd, "migration-workflows", "Manage BigQuery migration workflows",
