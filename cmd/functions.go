6ca0ce0b77e2dc4345c4c9988512e4dbb73b55d9
ZanNen <github3746@ashleygc.co.uk>
2026-07-13 10:44:58 +0100

    Register missing functions subcommands, drop stubbed tag (#616)

--- cmd/functions.go
+++ cmd/functions.go
@@ -4,7 +4,7 @@ import "github.com/spf13/cobra"
 
 // --- gcloud functions (#342) ---
 
-var functionsCmd = &cobra.Command{Use: "functions", Short: "Manage Cloud Functions (stubbed)"}
+var functionsCmd = &cobra.Command{Use: "functions", Short: "Manage Cloud Functions"}
 
 func init() {
 	for _, name := range []string{
@@ -14,5 +14,10 @@ func init() {
 	} {
 		registerStubCommand(functionsCmd, name, "Not yet implemented")
 	}
+		registerStubGroup(functionsCmd, "add-iam-policy-binding", "Manage add-iam-policy-binding", "list", "describe")
+	registerStubGroup(functionsCmd, "event-types", "Manage event-types", "list", "describe")
+	registerStubGroup(functionsCmd, "logs", "Manage logs", "list", "describe")
+	registerStubGroup(functionsCmd, "regions", "Manage regions", "list", "describe")
+	registerStubGroup(functionsCmd, "runtimes", "Manage runtimes", "list", "describe")
 	rootCmd.AddCommand(functionsCmd)
 }
