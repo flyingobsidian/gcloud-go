287fef9295107919820ebfb443dca0965efe892d
ZanNen <github3746@ashleygc.co.uk>
2026-07-19 14:42:23 +0100

    Implement developer-connect connections, insights-configs, and operations subcommands
    - cmd/developer_connect.go: replace the stub-only init with the top-level
      cobra command plus shared devConnLocationParent/devConnResourceName
      helpers.
    - cmd/developer_connect_connections.go: create/delete/describe/list/update
      against ProjectsLocationsConnections.
    - cmd/developer_connect_insights_configs.go: same five commands against
      ProjectsLocationsInsightsConfigs. InsightsConfigs.Patch does not accept
      an update mask so update just sends the full body.
    - cmd/developer_connect_operations.go: cancel/delete/describe/list/wait.
    - internal/gcp/clients.go: add DeveloperConnectService constructor.
    - cmd/developer_connect_test.go: subcommand-registration tests per group.
    
    Closes #1023
    Closes #1024
    Closes #1025

--- cmd/developer_connect.go
+++ cmd/developer_connect.go
@@ -1,15 +1,41 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
 
 // --- gcloud developer-connect (#330) ---
 
 var developerConnectCmd = &cobra.Command{Use: "developer-connect", Short: "Manage Developer Connect"}
 
 func init() {
-	crud := []string{"create", "delete", "describe", "list", "update"}
-	registerStubGroup(developerConnectCmd, "connections", "Manage connections", append(crud, "fetch-git-refs", "fetch-git-hashes", "fetch-linkable-git-repositories", "list-git-repositories")...)
-	registerStubGroup(developerConnectCmd, "insights-configs", "Manage insights configs", crud...)
-	registerStubGroup(developerConnectCmd, "operations", "Manage operations", "describe", "list", "cancel", "delete")
 	rootCmd.AddCommand(developerConnectCmd)
+}
+
+// devConnLocationParent returns "projects/PROJECT/locations/LOCATION".
+func devConnLocationParent(location string) (string, error) {
+	project, err := resolveProject()
+	if err != nil {
+		return "", err
+	}
+	if location == "" {
+		return "", fmt.Errorf("--location is required")
+	}
+	return fmt.Sprintf("projects/%s/locations/%s", project, location), nil
+}
+
+// devConnResourceName joins a parent, collection, and id into a fully qualified
+// name. If id is already fully qualified it is returned as-is.
+func devConnResourceName(location, collection, id string) (string, error) {
+	if strings.HasPrefix(id, "projects/") {
+		return id, nil
+	}
+	parent, err := devConnLocationParent(location)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s/%s/%s", parent, collection, id), nil
 }
